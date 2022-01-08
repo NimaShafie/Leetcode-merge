aefa643a8b3787713cda746898cdf6f05ed3a19b
Nima Shafie <nimzshafie@gmail.com>
2022-01-08 13:25:49 -0800

    problem solved

--- src/MedianOfTwoSortedArrays/App.java
+++ src/MedianOfTwoSortedArrays/App.java
@@ -3,8 +3,8 @@ package MedianOfTwoSortedArrays;
 public class App {
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
+        int[] nums1 = {1,2};
+        int[] nums2 = {3,4};
 
         MedianOfTwoSortedArrays solObj = new MedianOfTwoSortedArrays();
         Double medianSol = solObj.findMedianSortedArrays(nums1, nums2);
--- src/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
+++ src/MedianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
@@ -1,38 +1,70 @@
 package MedianOfTwoSortedArrays;
 
-import java.lang.Math;
+import java.util.Collections;
+import java.util.ArrayList;
 
 class MedianOfTwoSortedArrays {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 
-        int[] testNums = {2, 4, 6, 8};
-        double testAnswer = calcMedian(testNums);
+        //int[] testNums = {2, 4, 6, 8};
+        ArrayList<Integer> combNums = combArrays(nums1, nums2);
+        System.out.print("combNums == ");
+        for(int i = 0; i < combNums.size(); i++) System.out.print(combNums.get(i) + ", ");
+        double answer = calcMedian(combNums);
 
-        return testAnswer;
+        return answer;
     }
 
-    double calcMedian(int[] combNums) {
+    ArrayList<Integer> combArrays(int[] nums1, int[] nums2) {
+        int nums1Length = nums1.length;
+        int nums2Length = nums2.length;
+
+        ArrayList<Integer> newNums = new ArrayList<>((nums1Length + nums2Length));  // initalize the arrayList capacity in advance for better performance
+        if(nums1Length != 0) for(int i = 0; i < nums1Length; i++) newNums.add(nums1[i]);    // only add to the combined array if the original array has data in it
+        if(nums2Length != 0) for(int i = 0; i < nums2Length; i++) newNums.add(nums2[i]);
+        if(nums1Length == 0 && nums2Length == 0) return newNums;
+        Collections.sort(newNums);
+        return newNums;
+    }
+
+    double calcMedian(ArrayList<Integer> combNums) {
+        int medianPos;
         double median;
         double floorMean, cielMean;
+        if(combNums.size() == 1) return combNums.get(0);
         // odd -> divde by 2, round up
         // [2, 4, 6, 8, 9] 5/2 == 3, answer is 6
-        if(combNums.length % 2 != 0) {
-            System.out.println("Enter odds\n");
-            median = Math.ceil(combNums.length / 2);
+        if(combNums.size() % 2 != 0) {
+            System.out.println("\nEnter odds\n");
+            medianPos = combNums.size() / 2;
+            median = combNums.get(medianPos);
+            return median;
         }
 
         // even -> divide by 2, average number in that position with next higher number
         // [2, 4, 6, 8]
         // [4, 6]
         else {
-            int meanLength = combNums.length / 2;
-            System.out.println("Enter evens\n");
-            floorMean = combNums[meanLength - 1]; // 4
-            cielMean = combNums[(meanLength / 2) + 1]; // 6
+            int meanLength = combNums.size() / 2;
+            System.out.println("\nEnter evens\n");
+            floorMean = combNums.get(meanLength - 1); // 4
+            cielMean = combNums.get(meanLength); // 6
+            System.out.println("Floor Mean == " + floorMean + "\nCiel Mean == " + cielMean);
             System.out.println("Floor Mean = " + floorMean + ", cielMean = " + cielMean);
             median = ((floorMean + cielMean) / 2);
         }
-
         return median;
     }
 }
+
+// [1, 2, 3, 4]
+//  0  1  2  3
+// meanLength = size / 2 == (4/2) = 2
+// lowerBound == (meanLength - 1)
+// higherBound == (meanLength)
+
+// [1, 2, 3, 4, 5, 6, 7, 8]
+//  0  1  2  3  4  5  6  7
+// meanLength = size / 2 == (8/2) = 4
+// lowerBound == (meanLength - 1)
+// higherBound == (meanLength)
